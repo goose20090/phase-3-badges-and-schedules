9d2d292f0569b242be6df89e3fb8cde404c60951
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-13 16:10:06 +0000

    commit tree 939e59bb4f68069d52b7a318a313efc787a8972a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+# def batch_badge_creator namesArr
+#     namesArr.map do |name|
+#         badge_maker name
+#     end
+# end
+
+def batch_badge_creator namesArr
+    namesArr.map{|name| badge_maker name}
+end
+
+def assign_rooms speakersArr
+    speakersArr.map.with_index(1) do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer namesArr
+    batch_badge_creator(namesArr).each{|badge| puts badge}
+    assign_rooms(namesArr).each{|room| puts room}
+end
